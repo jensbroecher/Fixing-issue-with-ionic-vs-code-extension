99e204e785af507ba0c7cfb1c157e2b10526fbfb
jensbroecher <jensbroecher@gmail.com>
2023-05-26 17:38:56 +0200

    fixing error

---
+++ android/app/src/main/java/afridelivery/ionic/app/2023/MainActivity.java
@@ -0,0 +1,5 @@
+package afridelivery.ionic.app.2023;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'afridelivery.ionic.app.2023',
+  appName: 'afridelivery-ionic-app-2023',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
